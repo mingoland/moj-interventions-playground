10b66616e4a52ef9b7a76f4b805cffd03f60eafb
Ming Chan <mingosoft@gmail.com>
2020-08-13 19:03:31 +0100

    default data added

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -18,50 +18,8 @@ Example usage:
 */
 
 module.exports = {
-
-  // Insert values here
-  /*
-  users: {
-    'GB123456': {'server-user-name': 'Alex'}
-  },
-  */
-  var users = {
-    'GB123456' : 'Alex',
-    'EN000001' : 'Gary'
-  }
-
-  /*
-
-  {{ govukCheckboxes({
-    name: "vehicle-features",
-    fieldset: {
-      legend: {
-        text: "Which of these applies to your vehicle?"
-      }
-    },
-    hint: {
-      text: "Select all that apply"
-    },
-    items: [
-      {
-        value: "Heated seats",
-        text: "Heated seats",
-        id: "vehicle-features-heated-seats",
-        checked: checked("vehicle-features", "Heated seats")
-      },
-      {
-        value: "GPS",
-        text: "GPS",
-        id: "vehicle-features-gps",
-        checked: checked("vehicle-features", "GPS")
-      },
-      {
-        value: "Radio",
-        text: "Radio",
-        id: "vehicle-features-radio",
-        checked: checked("vehicle-features", "Radio")
-      }
-    ]
-  }) }}
-  */
+  users: [
+    {id: "GB123456", name: "Alex", phone: "09987654321"},
+    {id: "EN000001", name: "Bob", phone: "09987654321"}
+  ]
 }
--- app/routes.js
+++ app/routes.js
@@ -2,18 +2,4 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
-/*
-router.post('/pp-form-v01/making-referral-needs-and-risks', function(req, res){
-
-})
-*/
-/*
-router.get('/pp-form-v01/making-referral-needs-and-risks', function(req, res){
-  var users = {
-    'GB123456' : 'Alex',
-    'EN000001' : 'Gary'
-  }
-  res.render(users:users)
-})
-*/
 module.exports = router
